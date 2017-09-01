bbd892290449094ffd31e601cdfdc0718a0028e9
marcinsawicki <marcin.sawicki@lama-media.com>
2017-09-01 23:13:31 +0200

    push

--- api/controllers/ListController.js
+++ api/controllers/ListController.js
@@ -19,10 +19,57 @@ module.exports = {
     });
   },
 
-  delete: function(req, res, next) {
-    List.destroy(req.param('id')).exec(function(){
-      res.redirect('/user/account/'+req.session.me);
+  delete: function (req, res, next) {
+    List.findOne(req.param('id'), function findTask(err, list) {
+      if (err) return next(err);
+      if (!list) return next(err);
+
+      if (list.owner == req.session.me) {
+        List.destroy(req.param('id')).exec(function(){
+          res.redirect('/user/account/'+req.session.me);
+        });
+      } else {
+        req.flash('error', 'Oszalałeś?!');
+        res.redirect('/user/account/'+req.session.me);
+      }
     });
   },
+
+  edit: function (req, res, next) {
+    List.findOne(req.param('id'), function findTask(err, list) {
+      if (err) return next(err);
+      if (!list) return next(err);
+
+      res.view({
+        list: list
+      });
+    });
+  },
+
+  update: function(req, res, next) {
+    List.update(req.param('id'), req.params.all(), function listUpdated(err) {
+      if (err) {
+        return res.redirect('/list/edit/' + req.param('id'));
+      }
+
+      res.redirect('/user/account/' + req.session.me);
+    });
+  },
+
+  done: function(req, res, next) {
+    List.findOne(req.param('id'), function updateTask(err, list) {
+      if (err) return next(err);
+      if (!list) return next(err);
+
+      if (list.owner == req.session.me) {
+        List.update(req.param('id'),{status:'done'}).exec(function(){
+          res.redirect('/user/account/'+req.session.me);
+        });
+      } else {
+        req.flash('error', 'Oszalałeś?!');
+        res.redirect('/user/account/'+req.session.me);
+      }
+    });
+  }
 
 };
